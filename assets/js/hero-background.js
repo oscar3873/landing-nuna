9bebbad1095c5c79811e3884786dd67a34d1c5e8
Oscar Vargas <oscarvargas3873@gmail.com>
2024-01-03 20:15:48 -0300

    <fix>(Project):se cambio el orden de las imagenes en el hero

--- assets/js/hero-background.js
+++ assets/js/hero-background.js
@@ -3,7 +3,7 @@ const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
 
     const heroImg = d.getElementById("heroImg");
-    const heroPathImgArr = ["assets/img/background5.jpg", "assets/img/background6.jpg", "assets/img/background3.jpg", "assets/img/background4.jpg", "assets/img/background1.jpg", "assets/img/background2.jpg"];
+    const heroPathImgArr = ["assets/img/background1.jpg", "assets/img/background2.jpg", "assets/img/background3.jpg", "assets/img/background4.jpg", "assets/img/background5.jpg", "assets/img/background6.jpg"];
     
     let currentIndex = 0; // Iniciar el índice
     
